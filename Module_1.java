c47037cd3ec8ea523cce29239c40beb5bcafecd4
Zen1307 <hongyen180401@gmail.com>
2023-12-29 17:15:58 +0700

    Module_1

---
+++ Module_1.java
@@ -0,0 +1,24 @@
+package Module_1;
+
+public class Module_1 {
+static String hi = "hello world";
+static String hello = "hello";
+static String world = "world";
+static void printToScreen(String value){
+    System.out.println(value);
+}
+ //Question 1
+public static void main(String[] args){
+    System.out.println("hello world");
+ //Question 2
+    System.out.println(hi);
+ // Question 3
+    System.out.println(hello + " " + world);
+// Question 4
+printToScreen((hello + " " + world));
+}
+
+
+
+    
+}
